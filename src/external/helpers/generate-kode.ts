8951ac7c0fb0e9e90887850d959483e81693a12c
ng3m8ruz <mancunianmerchant816@gmail.com>
2023-03-06 20:35:04 +0700

    seconds

--- src/external/helpers/generate-kode.ts
+++ src/external/helpers/generate-kode.ts
@@ -1,8 +1,8 @@
 export const generateKode = (data: any) => {
-  const date: string = data['date'].replace('-', '')
+  const date: string = data['date'].replace('-', '').slice(2, -2)
   const resGenKode: number = data['resGenKode'] ?? 0
   const countGenKode: string = String(resGenKode + 1)
-  const totalGenKode: string = countGenKode.padStart(5, '0')
+  const totalGenKode: string = countGenKode.padStart(6, '0')
   const genKode: string = '0' + data['id'] + date + totalGenKode
   return new Promise<string>((resolve) => resolve(genKode))
 }
